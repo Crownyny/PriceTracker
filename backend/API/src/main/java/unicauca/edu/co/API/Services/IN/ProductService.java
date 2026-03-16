95e94885a2eccb969564a8b0bd530143028441e8
Santiago Escandon <santiescandon1@gmail.com>
2026-03-15 23:42:51 -0500

    se crean controllers Rest para enviar productos almacenados en BD

--- backend/API/src/main/java/unicauca/edu/co/API/Services/IN/ProductService.java
+++ backend/API/src/main/java/unicauca/edu/co/API/Services/IN/ProductService.java
@@ -1,6 +1,4 @@
 package unicauca.edu.co.API.Services.IN;
-
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +13,8 @@ import unicauca.edu.co.API.Config.WebSocketConfig;
 import unicauca.edu.co.API.DataAccess.Entity.NormalizedProductEntity;
 import unicauca.edu.co.API.DataAccess.Repository.ProductRepository;
 import unicauca.edu.co.API.Presentation.DTO.IN.QueryDTOIN;
-import unicauca.edu.co.API.Presentation.DTO.OUT.ExceptionDTO;
 import unicauca.edu.co.API.Presentation.DTO.OUT.NormalizedProductDTO;
 import unicauca.edu.co.API.Presentation.Mapper.NormalizedProductMapper;
-import unicauca.edu.co.API.Services.Events.NormalizedProductReceivedEvent;
 import unicauca.edu.co.API.Services.Interfaces.IN.IProductService;
 import unicauca.edu.co.API.Services.OUT.MessengerService;
 import unicauca.edu.co.API.Services.OUT.ScrapingService;
@@ -27,18 +23,10 @@ import unicauca.edu.co.API.Services.OUT.ScrapingService;
 @Service
 public class ProductService implements IProductService {
 
-    private final MessengerService messengerService;
     private static final Logger logger = LoggerFactory.getLogger(ProductService.class); 
     private final ScrapingService scrapingService;
     private final ProductRepository productRepository;
-    private final WebSocketConfig webSocket;
     private final NormalizedProductMapper mapperProduct;
-
-    //Se tendria que granulizar a otro servicio SearchService
-    //Por ahora lo dejo aqui para no complicar la inyeccion de dependencias, pero se puede refactorizar facilmente
-
-    private final ReferenceCheckService referenceCheckService;
-
 
     public ProductService(
         ScrapingService scrapingService, 
@@ -50,10 +38,7 @@ public class ProductService implements IProductService {
     ) {
         this.scrapingService = scrapingService;
         this.productRepository = productRepository;
-        this.webSocket = webSocket;
-        this.messengerService = messengerService;
         this.mapperProduct = mapperProduct;
-        this.referenceCheckService = referenceCheckService;
     }
 
     @Override
@@ -65,9 +50,9 @@ public class ProductService implements IProductService {
     }
 
     @Override
-    public List<NormalizedProductDTO> getProductByProductRef(String productRef) {
+    public List<NormalizedProductDTO> getProductByProductRef(QueryDTOIN query) {
         List<NormalizedProductDTO> products = null;
-        List<NormalizedProductEntity> entities = productRepository.findByProductRef(productRef);
+        List<NormalizedProductEntity> entities = productRepository.findByProductRef(query.getProduct_ref());
         if(entities != null && !entities.isEmpty()) {
             products = entities.stream()
                 .map(entity -> mapperProduct.toDTO(entity))
