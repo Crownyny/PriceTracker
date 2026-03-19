98cdda456aa6bf2680376cfca0fd615bab32da4f
Santiago Escandon <santiescandon1@gmail.com>
2026-03-19 10:13:56 -0500

    correccion controller get a post

--- backend/API/src/main/java/unicauca/edu/co/API/Presentation/Controller/ProductRestController.java
+++ backend/API/src/main/java/unicauca/edu/co/API/Presentation/Controller/ProductRestController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -26,7 +27,7 @@ public class ProductRestController {
     ) {
         this.productService = productService;
     }
-    @GetMapping("/search")
+    @PostMapping("/search")
     public List<NormalizedProductDTO> searchProductQuery(@RequestBody QueryDTOIN query) {
         return productService.getProductByProductRef(query);
     }
