abe0c8512f93377e4a3ec96c76e18ce648c723cd
Santiago Escandon <santiescandon1@gmail.com>
2026-03-28 16:03:59 -0500

    agrega documentacion de DTO

--- backend/API/src/main/java/unicauca/edu/co/API/Presentation/DTO/IN/IntentResponseDTOIN.java
+++ backend/API/src/main/java/unicauca/edu/co/API/Presentation/DTO/IN/IntentResponseDTOIN.java
@@ -7,6 +7,13 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+/**
+ * DTO de entrada para la respuesta de intención. Contiene el input del usuario, la probabilidad de compra, la etiqueta de intención y el umbral de decisión.
+ * @param input El texto de entrada del usuario.
+ * @param p_buy La probabilidad de que el usuario realice una compra.
+ * @param label La etiqueta de intención predicha por el modelo (BUY) (NOT_BUY).
+ * @param threshold El umbral de decisión utilizado para determinar si la intención es válida o no
+ */
 public class IntentResponseDTOIN {
     private String input;
     private String p_buy;
