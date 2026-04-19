55aea370184af271ae1eef20c661b4cdc9c5c448
maxskaink <mangelcvivas@unicauca.edu.co>
2026-04-18 20:51:06 -0500

    feat: refactor HistoryPriceRepository to use PriceHistoryEntity instead of ProductSnapShotEntity and remove ProductSnapShotEntity class

--- backend/API/src/main/java/unicauca/edu/co/API/DataAccess/Repository/HistoryPriceRepository.java
+++ backend/API/src/main/java/unicauca/edu/co/API/DataAccess/Repository/HistoryPriceRepository.java
@@ -6,11 +6,11 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import unicauca.edu.co.API.DataAccess.Entity.ProductSnapShotEntity;
+import unicauca.edu.co.API.DataAccess.Entity.PriceHistoryEntity;
 
 
 @Repository
-public interface HistoryPriceRepository extends JpaRepository<ProductSnapShotEntity, UUID>{
+public interface HistoryPriceRepository extends JpaRepository<PriceHistoryEntity, UUID>{
     /**
      * Listar HistoryEntity por productId
      *  y listar por fecha segun el rango de tiempo de range especificos
@@ -18,7 +18,7 @@ public interface HistoryPriceRepository extends JpaRepository<ProductSnapShotEnt
      *  3 semanas
      *  3 meses
      */
-    List<ProductSnapShotEntity> findByProductIdAndUpdatedAtAfter(
+    List<PriceHistoryEntity> findByProductIdAndRecordedAtAfter(
     UUID productId,
     LocalDateTime date);   
 
@@ -27,5 +27,5 @@ public interface HistoryPriceRepository extends JpaRepository<ProductSnapShotEnt
      * @param productId El ID del producto para el cual se desea obtener el historial de precios.
      * @return Un arreglo de ProductSnapShotEntity que contiene el historial de precios del producto.
      */
-    List<ProductSnapShotEntity> findByProductId(UUID productId);
+    List<PriceHistoryEntity> findByProductId(UUID productId);
 }
